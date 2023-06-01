043fb4474c6e3f53349be1fc0482dee156a859c5
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2023-06-01 02:51:29 -0500

    added profile and create to home page

--- public/uicode/homepageapp.ts
+++ public/uicode/homepageapp.ts
@@ -1,11 +1,30 @@
 import BaseApp from "./baseapp.js";
 import LoginHelper from "./loginhelper.js";
+import DocCreateHelper from "./doccreatehelper.js";
+import ProfileHelper from "./profilehelper.js";
 
 /** Guess app class */
 export class HomePageApp extends BaseApp {
     login = new LoginHelper(this);
+    documentCreate = new DocCreateHelper(this);
+    profileHelper = new ProfileHelper(this);
+    show_profile_modal: any = document.querySelector(".show_profile_modal");
+    show_create_modal: any = document.querySelector(".show_create_modal");
     /** */
     constructor() {
         super();
+        this.show_profile_modal.addEventListener("click", (event: any) => {
+            event.stopPropagation();
+            event.preventDefault();
+
+            this.profileHelper.show();
+        });
+        this.show_create_modal.addEventListener("click", (event: any) => {
+            event.stopPropagation();
+            event.preventDefault();
+
+            this.documentCreate.show();
+        });
+
     }
 }
