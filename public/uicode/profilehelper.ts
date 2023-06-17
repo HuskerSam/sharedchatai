3f457513103f1f81174074684b2ee8e78fe703d7
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2023-06-16 19:22:26 -0500

    changed delete and settings icons

--- public/uicode/profilehelper.ts
+++ public/uicode/profilehelper.ts
@@ -181,7 +181,10 @@ export default class ProfileHelper {
                                         style=""></div>
                                     <input type="file" class="file_upload_input" style="display:none;">
                                     <button class="profile_display_image_clear btn btn-secondary">
-                                        <i class="material-icons">delete</i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20">
+                                        <path fill="currentColor" d="M261-120q-24.75 0-42.375-17.625T201-180v-570h-41v-60h188v-30h264v30h188v60h-41v570q0 
+                                        24-18 42t-42 18H261Zm438-630H261v570h438v-570ZM367-266h60v-399h-60v399Zm166 0h60v-399h-60v399ZM261-750v570-570Z"/>
+                                    </svg>
                                     </button>
                                     <button class="profile_display_image_randomize btn btn-secondary">
                                         <i class="material-icons">casino</i>
