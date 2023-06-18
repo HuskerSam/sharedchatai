1f487e3fd22ac1cbca638fa2939c324e1ebb44ba
Mal402 <lhoang91@gmail.com>
2023-06-17 22:10:56 -0500

    merg

--- public/help/assets/js/theme.js
+++ public/help/assets/js/theme.js
@@ -98,10 +98,7 @@ $('#sidebarCollapse').on('click', function () {
 -------------------------- */
 
 $('.smooth-scroll,.idocs-navigation a').on('click', function() {
-
     var sectionTo = $(this).attr('href');
-	$('html, body').stop().animate({
-      scrollTop: $(sectionTo).offset().top - 120}, 1000, 'easeInOutExpo');
 });
 
 /*-----------------------------
