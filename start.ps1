cefe87d4088bea6de673c4d472aebb90f82b4b81
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2023-11-10 19:58:22 -0600

    transfer

--- start.ps1
+++ start.ps1
@@ -2,3 +2,4 @@ Start-Process Powershell.exe -Argumentlist "-file .\startfunctionwatch.ps1"
 Start-Process Powershell.exe -Argumentlist "-file .\startuicodewatch.ps1"
 Start-Process Powershell.exe -Argumentlist "-file .\startfirebase.ps1"
 code .
+Start-Process "http://localhost:5000/"
