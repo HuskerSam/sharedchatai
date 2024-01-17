d56b7ee0f766cfb292db5461ba3b5464212bfee2
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2024-01-16 22:11:39 -0600

    minor UI iteration

--- public/uicode/components/dialogvectorinspect.jsx
+++ public/uicode/components/dialogvectorinspect.jsx
@@ -18,37 +18,34 @@ export default function DialogVectorInspect(props) {
                 <Modal.Title>Test Pinecone</Modal.Title>
             </Modal.Header>
             <Modal.Body className="theme_panel">
-                <table class="embedding_options_table">
+                <table class="embedding_options_table" style={{width: "100%"}}>
                     <tr>
                         <td>Vector Id </td>
                         <td>
                             <FormControl as="input" onChange={(e) => setId(e.target.value)}></FormControl>
                         </td>
-                        <td></td>
-                        <td></td>
                     </tr>
                     <tr>
-                        <td colspan="3" style={{ textAlign: "right", padding: "8px" }}>
-                            <button class="btn btn-primary" onClick={async () => {
-                                let results = await props.fetchVector(id);
-                                setFetchResults(results);
-                            }}>Fetch Vector</button>
-                            <button class="btn btn-primary" onClick={async () => {
-                                let results = await props.deleteVector(id);
-                                setFetchResults(results);
-                            }}>Delete Vector</button>
-                        </td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td colspan="4">
-                            <div style={{whiteSpace: "pre-wrap"}}>{fetchResults}</div>
+                        <td colspan="2">
+                            <div style={{ whiteSpace: "pre-wrap", overflow: "auto", maxHeight: "40vh" }}>{fetchResults}</div>
                             <hr />
                         </td>
                     </tr>
                 </table>
             </Modal.Body>
             <Modal.Footer className="theme_panel">
+                <Button variant="secondary" onClick={async () => {
+                    let results = await props.fetchVector(id);
+                    setFetchResults(results);
+                }}>
+                    Fetch Vector
+                </Button>
+                <Button variant="secondary" onClick={async () => {
+                    let results = await props.deleteVector(id);
+                    setFetchResults(results);
+                }}>
+                    Delete Vector
+                </Button>
                 <Button variant="secondary" onClick={handleClose}>
                     Close
                 </Button>
