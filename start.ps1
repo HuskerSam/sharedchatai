2129c0b18e8bd69952987111ecb5a115c94bfcc2
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2024-01-27 17:32:18 -0600

    added nvm to set node version - should still work on other machines without nvm

--- start.ps1
+++ start.ps1
@@ -1,3 +1,4 @@
+nvm use 18.19.0
 Start-Process Powershell.exe -Argumentlist "-file .\startfunctionwatch.ps1"
 Start-Process Powershell.exe -Argumentlist "-file .\startuicodewatch.ps1"
 Start-Process Powershell.exe -Argumentlist "-file .\startfirebase.ps1"
