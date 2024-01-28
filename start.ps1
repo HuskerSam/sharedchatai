e9b2ab4f5ebfd4adaa2483771efb2756cbeb915b
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2024-01-27 18:10:22 -0600

    minor script fix

--- start.ps1
+++ start.ps1
@@ -3,5 +3,5 @@ Start-Process Powershell.exe -Argumentlist "-file .\startfunctionwatch.ps1"
 Start-Process Powershell.exe -Argumentlist "-file .\startuicodewatch.ps1"
 Start-Process Powershell.exe -Argumentlist "-file .\startfirebase.ps1"
 code .
-sleep(10)
+Start-Sleep(10)
 Start-Process "http://localhost:5000/"
