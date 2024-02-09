183c3fbec4e4eb25ea02596cac302a9fd599a558
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2024-02-09 16:52:23 -0600

    fix broken profile in unacog chat (image)

--- public/uicode/baseapp.ts
+++ public/uicode/baseapp.ts
@@ -117,7 +117,6 @@ export default class BaseApp {
       setTimeout(() => {
         this.credits_left = document.querySelector(".credits_left");
         this.signin_cta_navbar = document.querySelector(".signin_cta_navbar");
-        this.show_profile_modal = document.querySelector(".show_profile_modal");
         this.menu_profile_user_image_span = document.querySelector(".menu_profile_user_image_span");
         this.profile_menu_anchor = document.querySelector(".profile_menu_anchor");
         this.account_status_display = document.querySelector(".account_status_display");
@@ -136,15 +135,18 @@ export default class BaseApp {
           e.preventDefault();
           this.login.show();
         });
-        this.show_profile_modal.addEventListener("click", (event: any) => {
-          event.stopPropagation();
-          event.preventDefault();
-          this.profileHelper.show();
-        });
       }, 0);
     }
     if (this.standard_footer_bar_container) {
       createRoot(this.standard_footer_bar_container).render(React.createElement(ReactFooter));
+    }
+    this.show_profile_modal = document.querySelector(".show_profile_modal");
+    if (this.show_profile_modal) {
+      this.show_profile_modal.addEventListener("click", (event: any) => {
+        event.stopPropagation();
+        event.preventDefault();
+        this.profileHelper.show();
+      });
     }
     window.addEventListener("beforeinstallprompt", (e: any) => {
       e.preventDefault();
